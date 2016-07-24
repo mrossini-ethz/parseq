d3c6507d7c450c42bd1528fcf999fb4d816945c5
Marco Rossini <marco.rossini@phys.ethz.ch>
2016-07-24 07:46:22 +0000

    Improved passing of the rule arguments to the expander functions. However, the arguments still don't work correctly.

--- list-parser.lisp
+++ list-parser.lisp
@@ -32,7 +32,7 @@
           (setf ,,pos ,newpos)
           ,result))))
 
-(defun expand-atom (expr rule pos &rest args)
+(defun expand-atom (expr rule pos args)
   (cond
     ;; Is a quoted symbol
     ((quoted-symbol-p rule) (test-and-advance `(eql (nth ,pos ,expr) ',(second rule)) (second rule) pos))
@@ -43,11 +43,11 @@
     ((symbolp rule) (try-and-advance `(parse-list ',rule ,expr ,pos) pos))
     ))
 
-(defun expand-or (expr rule pos)
+(defun expand-or (expr rule pos args)
   `(cond
-     ,@(loop for r in rule collect (list (expand-rule expr r pos)))))
+     ,@(loop for r in rule collect (list (expand-rule expr r pos args)))))
 
-(defun expand-and (expr rule pos)
+(defun expand-and (expr rule pos args)
   ;; Create gensyms for the list of results, the individual result and the block to return from when short-circuiting
   (with-gensyms (list result block)
     ;; Block to return from when short-circuiting
@@ -57,42 +57,47 @@
          ;; Loop over the rules
          ,@(loop for r in rule for n upfrom 0 collect
                 ;; Bind a variable to the result of the rule expansion
-                `(let ((,result ,(expand-rule expr r pos)))
+                `(let ((,result ,(expand-rule expr r pos args)))
                    ;; If the result is nil, return nil
                    (unless ,result
                      (return-from ,block))
                    ;; Otherwise append the result
                    (appendf ,list ,result)))))))
 
-(defun expand-rule (expr rule pos &rest args)
+(defun expand-rule (expr rule pos args)
   ;; Rule is
   (cond
     ;; ... nil
-    ((null rule) (expand-atom expr nil pos))
+    ((null rule) (expand-atom expr nil pos nil))
     ;; ... an atom
-    ((atom rule) (apply #'expand-atom expr rule pos args))
+    ((atom rule) (expand-atom expr rule pos args))
     ;; ... a quoted symbol
-    ((quoted-symbol-p rule) (expand-atom expr rule pos))
+    ((quoted-symbol-p rule) (expand-atom expr rule pos args))
     ;; ... an OR expression
-    ((eql 'or (first rule)) (expand-or expr (rest rule) pos))
+    ((eql 'or (first rule)) (expand-or expr (rest rule) pos args))
     ;; ... an AND expression
-    ((eql 'and (first rule)) (expand-and expr (rest rule) pos))
+    ((eql 'and (first rule)) (expand-and expr (rest rule) pos args))
     ;; ... a call to another rule (with args)
-    ((symbolp (first rule)) (parse-list rule (rest expr) pos))
+    ((symbolp (first rule)) (try-and-advance `(parse-list ',rule ,expr ,pos) pos))
     ))
 
 (defmacro defrule (name lambda-list expr)
   (with-gensyms (x pos oldpos result)
     `(setf (gethash ',name *list-parse-rule-table*)
            #'(lambda (,x ,pos ,@lambda-list)
-               (let ((,oldpos ,pos) (,result ,(apply #'expand-rule x expr pos lambda-list)))
+               (let ((,oldpos ,pos) (,result ,(expand-rule x expr pos lambda-list)))
                  (if ,result
                    (values ,result t ,pos)
                    (values nil nil ,oldpos)))))))
 
 (defrule hello-world () (and 'hello 'world))
 (defrule hey-you () (and 'hey 'you))
+(defrule hey-x (x) (and 'hey x))
 (defrule test () (or hey-you hello-world))
+(defrule carg () 'x)
+(defrule varg (x) x)
 
+(parse-list 'carg '(x))
+(parse-list '(varg 'q) '(q))
 (parse-list 'test '(hey you))
 (parse-list 'test '(hello world))
