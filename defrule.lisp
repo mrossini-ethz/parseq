mrossini-ethz/parseq

commit 8666339fb7cb88f802780a62740773fb07ab772b

Replaced the tree position (tree pointer) implementation with an adjustable vector. Enforced thereby the abstraction in the code and allowed to enhance the efficiency in the future.

defrule.lisp
@@ -17,17 +17,17 @@
   ;; can be parsed by passing the start and/or end arguments.
   ;; The parse does fails if the end of the sequence is not reached
   ;; unless junk-allowed is given.
-  (let ((pos (list start)) (*terminal-failure-list* (cons '(-1) nil)))
+  (let ((pos (make-treepos start)) (*terminal-failure-list* (cons (make-treepos) nil)))
     ;; Attempt the parse
     (multiple-value-bind (result success newpos) (parseq-internal rule sequence pos)
       ;; Check for success and sequence bounds and return success or failure
-      (if (and success (or (and junk-allowed (or (null end) (< (first newpos) end))) (= (or end (length sequence)) (first newpos))))
+      (if (and success (or (and junk-allowed (or (null end) (< (treepos-highest newpos) end))) (= (or end (length sequence)) (treepos-highest newpos))))
           (values result t)
           (if parse-error
               (if (not success)
                   (let ((fail-pos (car *terminal-failure-list*)) (terminals (reverse (cdr *terminal-failure-list*))))
                     (f-error parse-match-error (:position fail-pos :terminals terminals)
-                             "Parse error: Expected ~{~s~^ or ~} at position ~{~a~^:~}." terminals fail-pos))
+                             "Parse error: Expected ~{~s~^ or ~} at position ~a." terminals fail-pos))
                   (f-error parse-junk-error () "Parse error: Junk at the end of the sequence starting at position ~{~a~^:~}." newpos))
               (values nil nil))))))
 
@@ -62,7 +62,7 @@
     `(if (treepos-valid ,pos ,expr)
          (if ,test
              (let ((,tmp ,result))
-               (setf ,pos (treepos-step ,pos ,inc))
+               (treepos-step! ,pos ,inc)
                (values ,tmp t))
              (push-terminal-failure ,pos ,terminal))
          (push-terminal-failure ,pos ,terminal))))
@@ -112,11 +112,11 @@
     ;; Is a character
     ((characterp arg) (runtime-match arg expr pos (and (characterp (treeitem pos expr)) (char= arg (treeitem pos expr))) arg))
     ;; Is a string and expression is also a string
-    ((and (stringp expr) (stringp arg)) (runtime-match arg expr pos (subseq-at arg (treeitem (butlast pos) expr) (last-1 pos)) arg (length arg)))
+    ((and (stringp expr) (stringp arg)) (runtime-match arg expr pos (subseq-at arg (treeitem (treepos-copy pos -1) expr) (treepos-lowest pos)) arg (length arg)))
     ;; Is a string, expression is not a string
     ((stringp arg) (runtime-match arg expr pos (and (stringp (treeitem pos expr)) (string= arg (treeitem pos expr))) arg))
     ;; Is a vector and expression is also a vector
-    ((and (vectorp expr) (vectorp arg)) (runtime-match arg expr pos (subseq-at arg (treeitem (butlast pos) expr) (last-1 pos)) arg (length arg)))
+    ((and (vectorp expr) (vectorp arg)) (runtime-match arg expr pos (subseq-at arg (treeitem (treepos-copy pos -1) expr) (treepos-lowest pos)) arg (length arg)))
     ;; Is a vector, expression is not a vector
     ((vectorp arg) (runtime-match arg expr pos (equalp arg (treeitem pos expr)) arg))
     ;; Is a number
@@ -145,19 +145,19 @@
     ;; Is a character
     ((characterp rule) `(test-and-advance ,rule ,expr ,pos (and (characterp (treeitem ,pos ,expr)) (char= (treeitem ,pos ,expr) ,rule)) (treeitem ,pos ,expr)))
     ;; Is a string
-    ((stringp rule) `(test-and-advance ,rule ,expr ,pos (if (stringp (treeitem (butlast ,pos) ,expr))
+    ((stringp rule) `(test-and-advance ,rule ,expr ,pos (if (stringp (treeitem (treepos-copy ,pos -1) ,expr))
                                                       ;; We are parsing a string, so match substring
-                                                      (subseq-at ,rule (treeitem (butlast ,pos) ,expr) (last-1 ,pos))
+                                                      (subseq-at ,rule (treeitem (treepos-copy ,pos -1) ,expr) (treepos-lowest ,pos))
                                                       ;; We are not parsing a string, match the whole item
                                                       (and (stringp (treeitem ,pos ,expr)) (string= (treeitem ,pos ,expr) ,rule)))
-                                       ,rule (if (stringp (treeitem (butlast ,pos) ,expr)) ,(length rule) 1)))
+                                       ,rule (if (stringp (treeitem (treepos-copy ,pos -1) ,expr)) ,(length rule) 1)))
     ;; Is a vector
-    ((vectorp rule) `(test-and-advance ,rule ,expr ,pos (if (vectorp (treeitem (butlast ,pos) ,expr))
+    ((vectorp rule) `(test-and-advance ,rule ,expr ,pos (if (vectorp (treeitem (treepos-copy ,pos -1) ,expr))
                                                       ;; We are parsing a vector, match the subsequence
-                                                      (subseq-at ,rule (treeitem (butlast ,pos) ,expr) (last-1 ,pos))
+                                                      (subseq-at ,rule (treeitem (treepos-copy ,pos -1) ,expr) (treepos-lowest ,pos))
                                                       ;; We are not parsing a vector, match the whole item
                                                       (equalp (treeitem ,pos ,expr) ,rule))
-                                       ,rule (if (vectorp (treeitem (butlast ,pos) ,expr)) ,(length rule) 1)))
+                                       ,rule (if (vectorp (treeitem (treepos-copy ,pos -1) ,expr)) ,(length rule) 1)))
     ;; Is a number
     ((numberp rule) `(test-and-advance ,rule ,expr ,pos (and (numberp (treeitem ,pos ,expr)) (= ,rule (treeitem ,pos ,expr))) ,rule))
     ;; Is a symbol
@@ -275,7 +275,7 @@
          (if (treepos-valid ,pos ,expr)
              (let ((,result (treeitem ,pos ,expr)))
                ;; Advance the position by one
-               (setf ,pos (treepos-step ,pos))
+               (treepos-step! ,pos)
                (values ,result t))
              (values nil nil))
          ;; Expression succeeded, which is bad
@@ -353,12 +353,12 @@
     `(when (and (treepos-valid ,pos ,expr) (funcall #',type-test (treeitem ,pos ,expr)))
        (let ((,length (treepos-length ,pos ,expr)))
          ;; Go into the list
-         (appendf ,pos 0)
+         (treepos-step-down! ,pos)
          (with-expansion-success ((,result ,success) ,expr ,rule ,pos ,args)
            ;; Success
-           (when (= (last-1 ,pos) ,length)
+           (when (= (treepos-lowest ,pos) ,length)
              ;; Step out of the list and increment the position
-             (setf ,pos (treepos-step (butlast ,pos)))
+             (setf ,pos (treepos-step (treepos-copy ,pos -1)))
              (values (list ,result) t))
            ;; Failure
            (values nil nil))))))
@@ -545,7 +545,7 @@
   ;; a closed over variable where the current position is pushed
   ;; on when a rule is called and popped when it returns.
   `(progn
-     (when (and ,stack (equal ,pos (first ,stack)))
+     (when (and ,stack (treepos= ,pos (first ,stack)))
        (f-error left-recursion-error () "Left recursion detected!"))
      ;; Save the position in which this rule was called
      (push (treepos-copy ,pos) ,stack)

test/test.lisp
@@ -660,13 +660,13 @@
   (check
     ;; parse-match-error
     ;; (and terminal-symbol (or (and 'b (* 'd) (parameter-terminal 'e)) (and 'c (or 'g 'h)) (and 'b 'f)))
-    (condition= (parseq 'parse-error '(v) :parse-error t) parse-match-error obj (equal (parse-error-position obj) '(0)) (equal (parse-error-terminals obj) '(a)))
-    (condition= (parseq 'parse-error '(a v) :parse-error t) parse-match-error obj (equal (parse-error-position obj) '(1)) (equal (parse-error-terminals obj) '(b c)))
-    (condition= (parseq 'parse-error '(a b v) :parse-error t) parse-match-error obj (equal (parse-error-position obj) '(2)) (equal (parse-error-terminals obj) '(d e f)))
-    (condition= (parseq 'parse-error '(a c v) :parse-error t) parse-match-error obj (equal (parse-error-position obj) '(2)) (equal (parse-error-terminals obj) '(g h)))
-    (condition= (parseq 'parse-error '(a b d v) :parse-error t) parse-match-error obj (equal (parse-error-position obj) '(3)) (equal (parse-error-terminals obj) '(d e)))
-    (condition= (parseq 'parse-error '(a b d d v) :parse-error t) parse-match-error obj (equal (parse-error-position obj) '(4)) (equal (parse-error-terminals obj) '(d e)))
-    (condition= (parseq 'parse-error '(a b d d d v) :parse-error t) parse-match-error obj (equal (parse-error-position obj) '(5)) (equal (parse-error-terminals obj) '(d e)))
+    (condition= (parseq 'parse-error '(v) :parse-error t) parse-match-error obj (treepos= (parse-error-position obj) (make-treepos 0)) (equal (parse-error-terminals obj) '(a)))
+    (condition= (parseq 'parse-error '(a v) :parse-error t) parse-match-error obj (treepos= (parse-error-position obj) (make-treepos 1)) (equal (parse-error-terminals obj) '(b c)))
+    (condition= (parseq 'parse-error '(a b v) :parse-error t) parse-match-error obj (treepos= (parse-error-position obj) (make-treepos 2)) (equal (parse-error-terminals obj) '(d e f)))
+    (condition= (parseq 'parse-error '(a c v) :parse-error t) parse-match-error obj (treepos= (parse-error-position obj) (make-treepos 2)) (equal (parse-error-terminals obj) '(g h)))
+    (condition= (parseq 'parse-error '(a b d v) :parse-error t) parse-match-error obj (treepos= (parse-error-position obj) (make-treepos 3)) (equal (parse-error-terminals obj) '(d e)))
+    (condition= (parseq 'parse-error '(a b d d v) :parse-error t) parse-match-error obj (treepos= (parse-error-position obj) (make-treepos 4)) (equal (parse-error-terminals obj) '(d e)))
+    (condition= (parseq 'parse-error '(a b d d d v) :parse-error t) parse-match-error obj (treepos= (parse-error-position obj) (make-treepos 5)) (equal (parse-error-terminals obj) '(d e)))
     ;; parse-junk-error
     (condition= (parseq 'parse-error '(a b f v) :parse-error t) parse-junk-error)))
 

utils.lisp
@@ -150,47 +150,95 @@
 
 ;; Tree position functions
 
-(defun treepos-valid (pos tree)
-  (when (and (sequencep tree) (not (minusp (first pos))))
-    (if (l> pos 1)
-        ;; Not toplevel
-        (when (> (length tree) (first pos))
-          ;; Descend into the toplevel item to recursively check the sublevel
-          (treepos-valid (rest pos) (elt tree (first pos))))
-        ;; Toplevel. Check whether the list is longer than the position index.
-        (> (length tree) (first pos)))))
+(defun make-treepos (&rest indices)
+  "Creates a new tree position object."
+  (let ((treepos (make-array 1 :element-type '(integer 0 #.most-positive-fixnum) :adjustable t :fill-pointer 0)))
+    (if indices
+        (dolist (idx indices)
+          (vector-push-extend idx treepos))
+        (vector-push 0 treepos))
+    treepos))
 
-(defun treeitem (pos tree)
-  (when (and (sequencep tree) (listp pos))
-    (cond
-      ((l> pos 1) (treeitem (rest pos) (elt tree (first pos))))
-      ((null pos) tree)
-      (t (elt tree (first pos))))))
+(defun treepos-copy (treepos &optional depth)
+  (let* ((n (length treepos)) (result (make-array n :element-type '(integer 0 #.most-positive-fixnum) :adjustable t :fill-pointer 0)))
+    (dotimes (i (if depth (if (minusp depth) (+ n depth) depth) n) result)
+      (vector-push (aref treepos i) result))))
 
-(defun treepos-length (pos tree)
-  (if (sequencep tree)
-     (if (l> pos 1)
-         (treepos-length (rest pos) (nth (first pos) tree))
-         (and (sequencep (elt tree (first pos))) (length (elt tree (first pos)))))
-     (f-error generic-parse-error () "Attempting to descend into a non-sequence type.")))
+(defun treepos-depth (treepos)
+  (length treepos))
 
-(defun treepos-step (pos &optional (delta 1))
-  (let ((newpos (copy-tree pos)))
-    (incf (car (last newpos)) delta)
+(defun treepos-valid (treepos tree)
+  "Verifies whether the given position exists in the given tree."
+  (labels ((recursion (pos idx tree)
+             (if (> (- (treepos-depth pos) idx) 1)
+                 ;; Not toplevel - recursively descend
+                 (when (> (length tree) (aref pos idx))
+                   (recursion pos (1+ idx) (elt tree (aref pos idx))))
+                 ;; Toplevel - check whether the list is longer than the position index
+                 (and (sequencep tree) (> (length tree) (aref pos idx))))))
+    (recursion treepos 0 tree)))
+
+(defun treeitem (treepos tree)
+  "Retrieves an item from the tree by using the given treepointer."
+  (labels ((recursion (pos idx tree)
+             (cond
+               ((> (- (treepos-depth pos) idx) 1) (recursion pos (1+ idx) (elt tree (aref pos idx))))
+               ((= (- (treepos-depth pos) idx) 1) (elt tree (aref pos idx)))
+               ((= (- (treepos-depth pos) idx) 0) tree))))
+    (recursion treepos 0 tree)))
+
+(defun treepos-length (treepos tree)
+  "Determines the length of the item in the tree pointed to by treepos."
+  (labels ((recursion (pos idx tree)
+             (unless (sequencep tree)
+               (f-error generic-parse-error () "Attempting to descend into a non-sequence type."))
+             (if (> (- (treepos-depth pos) idx) 1)
+                 (recursion pos (1+ idx) (elt tree (aref pos idx)))
+                 (let ((element (elt tree (aref pos idx))))
+                   (if (sequencep element)
+                       (length element)
+                       (f-error generic-parse-error () "Tree item is not a sequence."))))))
+    (recursion treepos 0 tree)))
+
+(defun treepos-step (treepos &optional (delta 1))
+  "Copies the tree pointer and increments the tree position."
+  (let ((n (length treepos)) (newpos (treepos-copy treepos)))
+    (incf (aref newpos (1- n)) delta)
     newpos))
 
-(defun treepos-copy (pos)
-  (copy-tree pos))
+(defun treepos-step! (treepos &optional (delta 1))
+  "Modifies the tree pointer, incrementing the tree position."
+  (let ((n (length treepos)))
+    (incf (aref treepos (1- n)) delta)
+    treepos))
+
+(defun treepos-step-down (treepos)
+  (let ((result (treepos-copy treepos)))
+    (vector-push-extend 0 result)
+    result))
+
+(defun treepos-step-down! (treepos)
+  (vector-push-extend 0 treepos)
+  treepos)
 
 (defun treepos> (a b)
-  (let ((na (list-length a)) (nb (list-length b)))
+  (let ((na (length a)) (nb (length b)))
     (if (= na nb)
-        (loop for ia in a for ib in b when (< ia ib) do (return nil) when (> ia ib) do (return t))
-        (and (loop for ia in a for ib in b always (= ia ib))
+        (loop for ia across a for ib across b when (< ia ib) do (return nil) when (> ia ib) do (return t))
+        (and (loop for ia across a for ib across b always (= ia ib))
              (> na nb)))))
 
 (defun treepos= (a b)
   (equalp a b))
+
+(defun treepos-elt (treepos index)
+  (elt treepos index))
+
+(defun treepos-highest (treepos)
+  (elt treepos 0))
+
+(defun treepos-lowest (treepos)
+  (elt treepos (1- (length treepos))))
 
 ;; Hash table functions
 

commit 293b63d1540d8abee8aedb506607c1eb05766262

Fixed a bug that was introduced in revision 8666339fb7cb88f802780a62740773fb07ab772b.

defrule.lisp
@@ -632,7 +632,7 @@
                    ;; Bind special variables from the (:let ...) option
                    (with-special-vars-from-options ,specials
                      ;; Save the previous parsing position
-                     (let ((,oldpos (treepos-copy ,pos)))
+                     (let ((,pos (treepos-copy ,pos)) (,oldpos (treepos-copy ,pos)))
                        ;; Print tracing information
                        (with-tracing (,name ,oldpos ,memo)
                          ;; Memoization (packrat parsing)