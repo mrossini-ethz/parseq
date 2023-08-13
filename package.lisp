d71fea58f613635b6abff3c55e2179dbc1e31aba
Marco Rossini <marco.rossini@protonmail.ch>
2023-08-13 15:09:27 +0200

    Removed the export of the ? symbol because it's not needed anymore.

--- package.lisp
+++ package.lisp
@@ -1,3 +1,10 @@
 (defpackage :parseq
   (:use :common-lisp)
-  (:export defrule parseq with-local-rules with-saved-rules clear-rules trace-rule untrace-rule ?))
+  (:export
+   defrule
+   parseq
+   with-local-rules
+   with-saved-rules
+   clear-rules
+   trace-rule
+   untrace-rule))
